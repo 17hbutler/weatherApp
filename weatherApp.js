dc5e585c055702e57b3842c8391cbd108fcfc01d
Harris <butlerha@mullenmail.com>
2016-01-07 08:36:30 -0700

    added new cdn

--- weatherApp.js
+++ weatherApp.js
@@ -1,2 +1,13 @@
-var weatherApp = angular.module('weatherApp', [])
+var myApp = angular.module('weatherApp', [])
 
+myApp.controller('homeController', ['$scope','cityService',
+                                    function($scope, cityService){
+                                    $scope.city=cityService.city;
+                                    $scope.$watch('city', 
+                                                  function() {
+                                        cityService.city = $scope.city; 
+                                        }) 
+                                    }]);
+myApp.service('cityService', function(){
+    this.city = "Denver, CO"; 
+}) 
